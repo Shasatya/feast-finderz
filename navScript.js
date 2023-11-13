bb1bc2241bb6a8cafb317f8fc2b23cce5cdf7f4e
Shasatya <120493084+Shasatya@users.noreply.github.com>
2023-11-14 01:20:40 +0530

    updated nav

--- navScript.js
+++ navScript.js
@@ -1,10 +1,6 @@
-//Smooth Scroll
-const scroll = new LocomotiveScroll({
-  el: document.querySelector(".main"),
-  smooth: true,
-});
-
 function openOverlay() {
+  document.body.style.overflow = "hidden";
+  document.body.style.userSelect = "none";
   document.getElementById("overlay").style.width = "100%";
   document.getElementsByClassName("toggle-btn")[0].style.fontSize = "0rem";
 }
